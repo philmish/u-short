d8b7ae96b6f7c449eb52952d190e8c4f2bfd7aa8
philmish <thantalosdream@protonmail.com>
2022-08-28 00:34:22 +0200

    feat: add main func for starting and gracefully shutting down server

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,40 @@
 package main
 
 import (
-    "log"
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/philmish/u-short/internal"
+	"golang.org/x/net/context"
 )
 
 func main() {
-    log.Println("u-short starting...")
+    done := make(chan os.Signal, 1)
+    signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+    srv := internal.DevServer()
+
+    go func() {
+        if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+           log.Fatalf("%s\n", err.Error()) 
+        }
+    }()
+    log.Println("Server started")
+    <-done
+
+    log.Println("Server stopped")
+    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+    defer func() {
+        cancel()
+    }()
+
+    if err := srv.Shutdown(ctx); err != nil {
+        log.Fatalf("Server shutdown failed: %s", err.Error())
+    }
+    log.Println("Server shutdown gracefully")
 }
--- internal/server.go
+++ internal/server.go
@@ -9,20 +9,23 @@ import (
 
 type ServerConf struct {
     Addr string
-    Handler RegexMatcher
+    Handler *RegexMatcher
     Middleware []middleware.Middleware
 }
 
-func (c ServerConf)DefaultServer() error {
+func (c ServerConf)DefaultServer() *http.Server {
     c.Handler.Add("/echo/[a-zA-Z0-9]{0,14}", middleware.ChainMiddleware(echo, c.Middleware...))
-    return http.ListenAndServe(c.Addr, c.Handler)
+    return &http.Server{
+        Addr: c.Addr,
+        Handler: c.Handler,
+    }
 }
 
 func commonMiddleware() []middleware.Middleware {
     return []middleware.Middleware{middleware.Logger}
 }
 
-func DevServer(port int) error {
+func DevServer(port int) *http.Server {
     ware := commonMiddleware()
     handler := NewRegexMatcher()
     conf := ServerConf{
